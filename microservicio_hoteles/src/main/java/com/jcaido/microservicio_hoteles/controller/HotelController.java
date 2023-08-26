a410f1d52886f48b0c16e39bcaea04def6369f9f
Cristobal Rosa <crisrogar@gmail.com>
2023-08-26 11:45:06 +0200

    feat: createHotel, obtainHotel, obtainAvailableHotel HotelController

---
+++ microservicio_hoteles/src/main/java/com/jcaido/microservicio_hoteles/controller/HotelController.java
@@ -0,0 +1,31 @@
+package com.jcaido.microservicio_hoteles.controller;
+
+import com.jcaido.microservicio_hoteles.model.Hotel;
+import com.jcaido.microservicio_hoteles.service.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HotelController {
+
+    @Autowired
+    HotelService hotelService;
+
+    public Hotel createHotel(Hotel hotel) {
+        return hotelService.createHotel(hotel);
+    }
+
+    @GetMapping(value = "hotels", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Hotel> obtainHotels() {
+        return hotelService.obtainHotels();
+    }
+
+    @GetMapping(value = "availableHotels", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Hotel> obtainAvailableHotels() {
+        return hotelService.obtainAvailableHotels();
+    }
+}
