3bff86f2ed57e36ba4d15b6875791cf25847f1c3
Cristobal Rosa <crisrogar@gmail.com>
2023-08-27 11:01:16 +0200

    feat: create microservice flights

---
+++ microservice_flights/src/main/java/com/jcaido/microservice_flights/MicroserviceFlightsApplication.java
@@ -0,0 +1,13 @@
+package com.jcaido.microservice_flights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceFlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceFlightsApplication.class, args);
+	}
+
+}
---
+++ microservice_flights/src/test/java/com/jcaido/microservice_flights/MicroserviceFlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcaido.microservice_flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceFlightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
