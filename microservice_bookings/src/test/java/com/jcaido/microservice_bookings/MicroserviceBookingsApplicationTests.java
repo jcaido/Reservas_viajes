ab85f4980e3a0f0dc3cfead7c8f9939641fd66f3
Cristobal Rosa <crisrogar@gmail.com>
2023-08-29 10:30:54 +0200

    feat: create microservice bookings

---
+++ microservice_bookings/src/test/java/com/jcaido/microservice_bookings/MicroserviceBookingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcaido.microservice_bookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBookingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
