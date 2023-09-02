2577e2e9bde9e57e06add8281f95aa2cf1e888c7
Cristobal Rosa <crisrogar@gmail.com>
2023-09-02 17:42:16 +0200

    feat: create booking BookServiceImpl microservice booking

--- microservice_bookings/src/main/java/com/jcaido/microservice_bookings/service/BookServiceImpl.java
+++ microservice_bookings/src/main/java/com/jcaido/microservice_bookings/service/BookServiceImpl.java
@@ -4,6 +4,7 @@ import com.jcaido.microservice_bookings.model.Book;
 import com.jcaido.microservice_bookings.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -13,9 +14,18 @@ public class BookServiceImpl implements BookService{
     @Autowired
     BookRepository bookRepository;
 
+    @Autowired
+    RestTemplate restTemplate;
+
+    String url = "http://servicio-flights";
+
     @Override
     public Book createBooking(Book book, int totalPersons) {
-        return null;
+
+        bookRepository.save(book);
+        restTemplate.put(url + "/flights/{idFlight}/{places}", null, book.getFlight(), totalPersons);
+
+        return book;
     }
 
     @Override
