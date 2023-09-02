fbb3081090e18480faca063466f815a62966f872
Cristobal Rosa <crisrogar@gmail.com>
2023-09-02 17:41:12 +0200

    feat: create java bean RestTemplate

--- microservice_bookings/src/main/java/com/jcaido/microservice_bookings/MicroserviceBookingsApplication.java
+++ microservice_bookings/src/main/java/com/jcaido/microservice_bookings/MicroserviceBookingsApplication.java
@@ -2,9 +2,16 @@ package com.jcaido.microservice_bookings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MicroserviceBookingsApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceBookingsApplication.class, args);
